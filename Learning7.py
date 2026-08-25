24d0ba181029e96a84ebbb3c8480178216042161
Sohail.conCat <Sohail.conCat@gmail.com>
2026-08-25 13:58:53 +0530

    Fix formatting and improve comments in Learning7.py

--- Learning7.py
+++ Learning7.py
@@ -6,7 +6,7 @@ game =[[0, 0, 0],
 def game_play(player_move=0, row=0, column=0, just_display=False):
 	print("   a  b  c")
 	if not just_display:
-		game[row][column]=player_move #if just_display = True then skips and it just displays
+		game[row][column]=player_move #if just_display = True then skip this step and it just displays
 	for count, row in enumerate(game):
 			print(count, row)
 
